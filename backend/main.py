e24f050c10d53a6fa3fc988f90f1b5550124e2b2
Darko Djurdjev <godzilla@Darkos-MacBook-Pro.local>
2026-05-30 00:30:48 +0200

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,11 @@ app = FastAPI(title="Movie Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://movie-tracker-ddpol8qs3-darko-s-projects2.vercel.app"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://movie-tracker-git-main-darko-s-projects2.vercel.app",
+    "https://movie-tracker-ddpol8qs3-darko-s-projects2.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
